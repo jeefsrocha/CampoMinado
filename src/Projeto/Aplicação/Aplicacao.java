b7cae040bc69353dc431a4b3a76cc8c296dcfae9
jeefsrocha <115155851+jeefsrocha@users.noreply.github.com>
2024-06-11 20:38:07 -0300

    Alterando linhas e colunas

--- src/Projeto/Aplicação/Aplicacao.java
+++ src/Projeto/Aplicação/Aplicacao.java
@@ -8,7 +8,7 @@ import Projeto.visao.TabuleiroConsole;
 public class Aplicacao {
     public static void main (String[] args)
 {
-    Tabuleiro tabuleiro = new Tabuleiro(9, 9, 10);
+    Tabuleiro tabuleiro = new Tabuleiro(7, 5, 10);
 
     new TabuleiroConsole(tabuleiro);
 
